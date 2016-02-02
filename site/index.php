ec2f571501a1a2131011be5a272d29d2bd43f5dd
Ted Zhu <tedz2usa@gmail.com>
2016-02-02 05:57:50 -0500

    New favicon, and basic login page.

--- site/index.php
+++ site/index.php
@@ -13,8 +13,8 @@ echo 'Connected';
 pre($_SESSION);
 
 $settings = get_app_settings();
+$users = get_app_users();
 
-pre($settings);
 
 if (is_logged_in()) {
 	echo 'You are logged in!';
@@ -40,7 +40,8 @@ function get_app_settings() {
 
 	$settings = array(
 		// Default values.
-		'application_name' => 'My Chatroom'
+		'application_name' => 'My Chatroom',
+		'login_title' => 'Team Collaboration'
 	);
 
 	$res = $mysqli->query('SELECT * FROM settings');
@@ -51,14 +52,72 @@ function get_app_settings() {
 	return $settings;
 }
 
+function get_app_users() {
+	global $mysqli;
+	$users = [];
+	$res =$mysqli->query('SELECT * FROM users');
+	while ( $row = $res->fetch_assoc() ) {
+		$users[] = $row;
+	}
+	return $users;
+}
+
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<link rel='shortcut icon' href='favicon.ico' />
 		<title><?php echo $settings['application_name']; ?></title>
+		<link rel='stylesheet' href='styles.css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
+		<div class='main'>
+
+		<?php 
+
+		if (!is_logged_in()) {
+
+			?>
+			<div class='login'>
+				<h1><?php echo $settings['login_title']; ?></h1>
+				<p class='login-caption'>Please Log In</p>
+				<div class='login-userlist'>
+				<?php
+				foreach ($users as $user) {
+					?>
+					<div class='login-userlist-user'>
+						<div class='login-userlist-user-pic'>
+						</div>
+						<div class='login-userlist-user-name'><?php echo $user['first_name'] . ' ' . $user['last_name'] ?></div>
+					</div>
+					<?php
+				}
+				?>
+				</div>
+			</div>
+			<?php
+
+
+		} else {
+
+		}
+
+		?>
+
+
+		</div>
+
+<br><br><br><br>
+<br><br><br><br>
+<br><br><br><br>
+
+<?php
+
+pre($settings);
+pre($users);
+
+?>
 		
 	</body>
 </html>
